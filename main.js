dd5924e7b66c5182786ecc7b25ea14ede52aa933
Przemysław Golec <golec.przemyslaw@gmail.com>
2017-05-16 13:35:57 +0200

    Product details, layout and fixed scripts

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import {Router, Route, browserHistory} from 'react-router'
 
 import App from './components/App'
+import ProjectDetails from './components/ProjectDetails'
 
 ReactDOM.render((
-    <Router history = {browserHistory}>
-      <Route path = "/" component = {App} />
-    </Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/projekt/:name" component={ProjectDetails}/>
+  </Router>
 ), document.getElementById('app'))
